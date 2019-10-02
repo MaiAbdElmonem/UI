5da214654ecfee57e27d36959f1339a19550df4d
Mai Abd Elmonem <mai.abdelmonem@admins-MacBook-Pro-3.local>
2019-10-02 17:37:20 +0200

    commit

---
+++ Ui/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Ui
+//
+//  Created by Mai Abd Elmonem on 10/2/19.
+//  Copyright © 2019 Mai Abd Elmonem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
