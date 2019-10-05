9946f2d474ef05764446f15c83692f82c874e5dd
mai ahmed <maiahmed@192.168.220.128>
2019-10-05 09:19:47 -0700

    full

---
+++ Ui/ImageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCollectionViewCell.swift
+//  Ui
+//
+//  Created by mai ahmed on 10/5/19.
+//  Copyright © 2019 Mai Abd Elmonem. All rights reserved.
+//
+
+import UIKit
+
+class ImageCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+}
--- Ui/ViewController.swift
+++ Ui/ViewController.swift
@@ -8,9 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource{
+    
+    @IBOutlet weak var collectionView: UICollectionView!
 
     @IBOutlet weak var AppleImage: UIImageView!
+    
+    var imageArr = [UIImage(named: "apple"), UIImage(named: "apple-1"), UIImage(named: "user")]
+    var textArr = [" is simply dummy text "," is simply text ", " is dummy text "]
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -19,7 +28,21 @@ class ViewController: UIViewController {
      self.AppleImage.layer.masksToBounds = true
 //        self.AppleImage.layer.borderWidth = 3.0;
 //        self.AppleImage.layer.borderColor = [UIColor whiteColor].CGColor;
+        self.collectionView.delegate = self
+        self.collectionView.dataSource = self
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return imageArr.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "icv", for: indexPath) as! ImageCollectionViewCell
+        cell.image.image = imageArr[indexPath.row]
+        cell.label.text = textArr[indexPath.row]
+        return cell
+    }
+    
 
 
 }
